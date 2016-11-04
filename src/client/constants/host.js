02b80d4f4acc77365d668932f7265781fef5b385
Sean Smith <sean.smith.2009@gmail.com>
2016-11-03 22:04:38 -0700

    updatd

--- src/client/constants/host.js
+++ src/client/constants/host.js
@@ -1,3 +1,3 @@
 
-export const PROD_HOST_URL = 'GITHUB_CALLBACK_URL_PROD=https://murmuring-chamber-37829.herokuapp.com';
-export const DEV_HOST_URL='http://127.0.0.1:3000';
+export const PROD_HOST_URL = 'https://murmuring-chamber-37829.herokuapp.com';
+export const DEV_HOST_URL= 'http://127.0.0.1:3000';
